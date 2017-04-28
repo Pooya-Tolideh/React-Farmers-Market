07bd1c1dccc487838e1aa2d8cc7e7d385398e584
pooya-tolideh <pooya.tolideh@gmail.com>
2017-04-28 19:48:55 -0400

    feat :: added inventory,  order and header components - header accepts a prop as its tagline

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,36 @@ const ReactDOM = require('react-dom');
 
 
 
+//<Inventory/>
+const Inventory = React.createClass({
+    render : () => {
+        return(
+            <p>Inventory</p>
+        )
+    }
+});
+
+//<Order/>
+const Order = React.createClass({
+    render : () => {
+        return(
+            <p>Order</p>
+        )
+    }
+});
+
+//<Header/>
+const Header = React.createClass({
+    render : function () {
+        return(
+            <header className="top">
+                <h1>Catch of the Day</h1>
+                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+            </header>
+        )
+    }
+});
+
 
 
 // <StorePicker/>
