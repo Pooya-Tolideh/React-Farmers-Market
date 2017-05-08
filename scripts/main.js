5fbd4faf1d7824fd9d88a43d27c85d41c9ac7791
pooya-tolideh <pooya.tolideh@gmail.com>
2017-05-08 13:55:43 -0400

    rendered items in the state

--- scripts/main.js
+++ scripts/main.js
@@ -9,17 +9,110 @@ const Route = ReactRouter.Route;
 const Navigation = ReactRouter.Navigation;
 const History = ReactRouter.History;
 
+var sampleData = {
+    item1: {
+            name : 'rand',
+            price : 9.99,
+            desc : 'lorem ipsom',
+            status: 'Fresh',
+            image: './images/test.jpg'
+
+        },
+
+    item2: {
+            name : 'wand',
+            price : 9.99,
+            desc : 'lorem ipsom',
+            status: 'Fresh',
+            image: './images/test.jpg'
+
+        },
+
+    item3: {
+            name : 'band',
+            price : 9.99,
+            desc : 'lorem ipsom',
+            status: 'Sold Out',
+            image: './images/test.jpg'
+
+        }
+};
+
 //<App/>
 const App = React.createClass({
-    render: () => {
+    //parent component that controls data flow has state object
+    // initial state = inital state of data in the app before it mounts
+
+    getInitialState : function() {
+        return {
+            items: sampleData,
+            orders: {}
+        }
+    },
+
+    addItem : function(item) {
+        var timestamp = (new Date()).getTime();
+        this.state.items[`item-${timestamp}`] = item;
+        this.setState({ items: this.state.items });
+    },
+
+    // get the key-value pair in orders dataset
+    // if it's empty set the value of that key to 1
+    // else add 1 to it
+    addOrder: function(key) {
+        this.state.orders[key] = this.state.orders[key] + 1 || 1;
+        this.setState({ orders: this.state.orders });
+    },
+
+    renderItems : function () {
+        let list = [];
+        for (let key in this.state.items) {
+            list.push(<Item key={key} details={this.state.items[key]} index={key} addOrder={this.addOrder}/>)
+        }
+        return list;
+    },
+
+
+    render: function () {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Farmers Market"/>
+                    <ul>
+                        {this.renderItems()}
+                    </ul>
                 </div>
                 <Order/>
-                <Inventory/>
+                <Inventory addItem={ this.addItem } />
             </div>
+        )
+    }
+});
+
+
+//<Fish />
+
+const Item = React.createClass({
+
+    createOrder: function(e) {
+        e.preventDefault();
+        this.props.addOrder(this.props.index);
+    },
+    
+    render: function() {
+        const details = this.props.details;
+        const isAvailable = (details.status === 'Fresh') ? true : false;
+        const btn = isAvailable ? 'Add to Order' : 'Sold Out';
+        return (
+            <li className="menu-fish">
+                <img src={details.image} alt={details.name}/>
+                <h3 className="fish-name">
+                    {details.name}
+                    <span className="price">{details.price}</span>
+                    <p>{details.desc}</p>
+                </h3>
+                <button disabled={!isAvailable} onClick={this.createOrder}>{btn}</button>
+            </li>
         )
     }
 });
@@ -28,17 +121,19 @@ const App = React.createClass({
 
 //<Inventory/>
 const Inventory = React.createClass({
-    render : () => {
+    render : function () {
         return(
             <div>
                 <p>Inventory</p>
-                <AddForm />
+                <AddForm {...this.props} />
             </div>
         )
     }
 });
 
+
 const AddForm = React.createClass({
+
     createItem: function(e) {
         e.preventDefault();
         var item = {
@@ -49,14 +144,16 @@ const AddForm = React.createClass({
             image : this.refs.image.value
         };
         console.log(item);
+        this.props.addItem(item);
     },
+
     render: function() {
         return (
             <form className="fish-edit" onSubmit={this.createItem}>
                 <input type="text" ref="name" placeholder="Name"/>
                 <input type="text" ref="price" placeholder="Price"/>
                 <select ref="status">   
-                    <option value="available">Fresh!</option>
+                    <option value="available">Fresh</option>
                     <option value="unavailable">Sold Out!</option>
                 </select>
                 <textarea name="text" id="" ref="desc" placeholder="Desc"></textarea>
@@ -68,6 +165,8 @@ const AddForm = React.createClass({
 });
 
 
+
+
 //<Order/>
 const Order = React.createClass({
     render : () => {
@@ -76,6 +175,9 @@ const Order = React.createClass({
         )
     }
 });
+
+
+
 
 //<Header/>
 const Header = React.createClass({
